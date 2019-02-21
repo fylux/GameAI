94664f7cdedd352fceac7ce911ec64b9238f8245
Jaime <jaime.m.legaz.306@gmail.com>
2019-02-21 12:32:37 +0100

    Face + LWYG
    > Creados pero no funcionales, a falta de la correspondiente llamada a Align

---
+++ Steerings/Face.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : SteeringBehaviourTarget {
+
+
+    override
+    public Steering Steer()
+    {
+        Steering steering = new Steering();
+
+        Vector3 direction = target.position - body.position;
+
+        if (direction.magnitude <= 0.0f)
+            return steering;
+
+        float targetOrientation = Mathf.Atan2(direction.x, direction.z);
+        targetOrientation *= Mathf.Rad2Deg;
+
+        // virtualTarget.GetComponent<Body>.orientation = targetOrientation
+        // return Align.Steer(...), siendo el target con el que nos vamos a alinear el virtualTarget
+
+        return steering;
+    }
+
+
+}
---
+++ Steerings/LWYG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LWYG : SteeringBehaviourTarget {
+
+    [SerializeField]
+    private float maxPrediction;
+
+    public override Steering Steer()
+    {
+        Steering steering = new Steering();
+
+        Vector3 direction = target.position - body.position;
+        float distance = direction.magnitude;
+        float speed = body.velocity.magnitude;
+
+        float prediction;
+        if (speed <= distance / maxPrediction)
+            prediction = maxPrediction;
+        else
+            prediction = distance / speed;
+
+        Vector3 pred_target = target.position + (target.velocity * prediction);
+
+        // return Face.Steer(...), siendo el target nuestro pred_target
+
+        return steering;
+    }
+}

94e02cf766a8112dce199a36584d62769bbdcc23
Jaime <jaime.m.legaz.306@gmail.com>
2019-03-10 14:16:41 +0100

    Formations finished

--- Formations/SquarePattern.cs
+++ Formations/SquarePattern.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossPattern : FormationPattern {
+public class SquarePattern : FormationPattern {
 
+    [SerializeField]
+    int columns = 3;
 
     private void Awake()
     {
@@ -43,27 +45,20 @@ public class CrossPattern : FormationPattern {
 
     public override Location GetSlotLocation(int slotIndex)
     {
-        float xval = 1;
-        float zval = 1;
-        if (slotIndex < 2)
-            xval = -1;
-        if (slotIndex == 1 || slotIndex == 3)
-            zval = -1;
+        slotIndex++;
 
         Location location = new Location();
 
-        location.position.x = 2 * characterRadius * xval;
-        location.position.z = 2 * characterRadius * zval;
+        location.position.x = (slotIndex % columns) * characterRadius * 2;
+        location.position.z = (slotIndex / columns) * characterRadius * -2;
 
         location.orientation = leader.orientation;
-
-       // Debug.Log(slotIndex + " -> " + location.position); // Devuelve siempre el (1.0, 0.0, 0.0)
 
         return location;
     }
 
     public override bool SupportsSlots(int slotCount)
     {
-        return slotCount <= numOfSlots;
+        return true;
     }
 }

a6b6c53f2e74a9acf79214a78e917b3559d87749
Alfonso <alfonso.cama@hotmail.com>
2019-03-31 00:19:03 +0100

    Only render Minimap every N seconds

---
+++ Util/ManualCameraRender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ManualCameraRender : MonoBehaviour {
+    private float nextActionTime = 0.0f;
+    public float period = 1f;
+    Camera cam;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+        cam.enabled = false;
+    }
+
+    void Update() {
+        if (Time.time > nextActionTime) {
+            cam.Render();
+            nextActionTime += period;
+        }
+    }
+}

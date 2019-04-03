9fa3bd59906df3879c3b69ce4f14a70cd151becb
Jaime <jaime.m.legaz.306@gmail.com>
2019-04-03 11:02:55 +0200

    Implemented WeightDefHalf
    > No testing

--- Strategy/InfoManager.cs
+++ Strategy/InfoManager.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 
 public class InfoManager : MonoBehaviour {
 
-    Map map;
+    public Map map;
 
-    [SerializeField]
-    GameObject mid, top,  bottom;
+    
+    public GameObject mid, top, bottom;
+    public Dictionary<string, GameObject> waypoints = new Dictionary<string, GameObject>();
+    public Dictionary<GameObject, Node> waypointNode = new Dictionary<GameObject,Node>();
 
     [SerializeField]
     Faction faction;
@@ -18,7 +20,7 @@ public class InfoManager : MonoBehaviour {
     [SerializeField]
     float areaSize, sphereSize;
 
-    public Vector3 position;
+   // public Vector3 position;
 
     [SerializeField]
     Body allyBase, enemyBase;
@@ -27,13 +29,32 @@ public class InfoManager : MonoBehaviour {
 
     void Start () {
         map = GameObject.Find("Terrain").GetComponent<Map>();
+
+        waypoints.Add("mid", mid);
+        waypoints.Add("top", top);
+        waypoints.Add("bottom", bottom);
+
+        waypoints.Add("upMid",mid.transform.Find("UpMid").gameObject);
+        waypoints.Add("downMid", mid.transform.Find("DownMid").gameObject);
+
+        waypoints.Add("upTop", top.transform.Find("UpTop").gameObject);
+        waypoints.Add("downTop", top.transform.Find("DownTop").gameObject);
+
+        waypoints.Add("upBottom", bottom.transform.Find("UpBottom").gameObject);
+        waypoints.Add("downBottom", bottom.transform.Find("DownBottom").gameObject);
+
+        foreach (KeyValuePair<string, GameObject> entry in waypoints)
+        {
+            waypointNode.Add(entry.Value, map.NodeFromPosition(entry.Value.transform.position));
+            Debug.Log("Añadido el waypoint " + entry.Value + " en la posicion " + map.NodeFromPosition(entry.Value.transform.position));
+        }
     }
 
-   /* private void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(allyBase.position, sphereSize);
-    }*/
+        Gizmos.DrawSphere(waypoints["upBottom"].transform.position, sphereSize);
+    }
 
     void Update () {
 
@@ -58,14 +79,40 @@ public class InfoManager : MonoBehaviour {
         for (int i = 0; i < found; i++) {
             AgentUnit agent = hits[i].GetComponent<AgentUnit>();
             units.Add(agent);
-            Debug.Log("Encontrada una unidad: " + agent);
+          //  Debug.Log("Encontrada una unidad: " + agent);
         }
 
         return units;
     }
 
-    public HashSet<AgentUnit> GetUnitsArea(Node tile, int layerMask) {
+    public HashSet<AgentUnit> GetUnitsArea(Node tile, int layerMask)
+    {
         return GetUnitsArea(tile, areaSize, layerMask);
+    }
+
+    public HashSet<AgentUnit> GetUnitsFactionArea(Node tile, float areaSize, int layerMask, Faction fact)
+    {
+        HashSet<AgentUnit> units = new HashSet<AgentUnit>();
+
+        int found = Physics.OverlapSphereNonAlloc(tile.worldPosition, areaSize, hits, layerMask);
+
+        for (int i = 0; i < found; i++)
+        {
+            AgentUnit agent = hits[i].GetComponent<AgentUnit>();
+          //  Debug.Log("Encontrada una unidad: " + agent);
+            if (agent.faction == fact)
+            {
+                units.Add(agent);
+                
+            }   
+        }
+
+        return units;
+    }
+
+    public HashSet<AgentUnit> GetUnitsFactionArea(Node tile, int layerMask, Faction fact)
+    {
+        return GetUnitsFactionArea(tile, areaSize, layerMask, fact);
     }
 
     // Obtiene el numero de uniades aliadas que siguen esa estrategia en un area
@@ -81,21 +128,21 @@ public class InfoManager : MonoBehaviour {
         else
             nodo = map.NodeFromPosition(enemyBase.position);
 
-        HashSet<AgentUnit> units = GetUnitsArea(nodo, areaSize, 9);
-        HashSet<AgentUnit> unitsFound = new HashSet<AgentUnit>(units.Where(u => u.faction == unitsFaction));
+        HashSet<AgentUnit> units = GetUnitsFactionArea(nodo, areaSize, 1 << 9, unitsFaction);
+       // HashSet<AgentUnit> unitsFound = new HashSet<AgentUnit>(units.Where(u => u.faction == unitsFaction));
 
-        foreach (AgentUnit unit in unitsFound) {
+       /* foreach (AgentUnit unit in units) { //No necesario con el enfoque de multiples distancias
             unitsFound.Union(GetUnitsArea(map.NodeFromPosition(unit.position), 5)
                             .Where(u => u.faction == unitsFaction));
-        }
+        }*/
 
-        return unitsFound;
+        return units;
     }
 
     // Obtiene un numero que indica la ventaja militar en un area
-    public void MilitaryAdvantage(Node tile, int layerMask)
+    public float MilitaryAdvantage(Node tile, float areaSize, int layerMask, Faction fact)
     {
-        HashSet<AgentUnit> units = GetUnitsArea(tile, layerMask);
+        HashSet<AgentUnit> units = GetUnitsArea(tile, areaSize, layerMask);
 
         Vector2 number = new Vector2(0,0);
         Vector2 avgHP = new Vector2(0, 0);
@@ -104,13 +151,17 @@ public class InfoManager : MonoBehaviour {
         Vector2 ranged = new Vector2(0, 0);
         Vector2 scouts = new Vector2(0, 0);
         Vector2 artill = new Vector2(0, 0);
+        Vector2 dmpf = new Vector2(0, 0); // Daño medio por frame (ignorando tablas, generalización)
+        Vector2 totalHP = new Vector2(0, 0);
 
         foreach (AgentUnit unit in units) {
             int i = unit.faction == Faction.A ? 0 : 1;
 
             number[i]++;
             avgHP[i] += unit.health;
-            avgATK[i] += 1; // TODO Modificar cuando se añada como atributo
+            totalHP[i] += unit.health;
+            avgATK[i] += unit.attack;
+            dmpf[i] += unit.attack;
             if (unit is Melee)  melee[i]++;
             else if (unit is Ranged) ranged[i]++;
             else if (unit is Scout) scouts[i]++;
@@ -120,14 +171,23 @@ public class InfoManager : MonoBehaviour {
             avgHP[0] /= number[0];
             avgATK[0] /= number[0];
         }
-        
-        Debug.Log("Numero de unidades de A: " + number[0] + ", y de B: " + number[1]);
-        Debug.Log("HP average de A: " + avgHP[0] + ", y de B: " + avgHP[1]);
-        Debug.Log("ATK average de A: " + avgATK[0] + ", y de B: " + avgATK[1]);
-        Debug.Log("Melees de A: " + melee[0] + ", rangeds: " + ranged[0] + ", scouts: " + scouts[0] + ", y artilleria: " + artill[0]);
-        Debug.Log("Melees de B: " + melee[1] + ", rangeds: " + ranged[1] + ", scouts: " + scouts[1] + ", y artilleria: " + artill[1]);
+
+        /* Debug.Log("Numero de unidades de A: " + number[0] + ", y de B: " + number[1]);
+         Debug.Log("HP average de A: " + avgHP[0] + ", y de B: " + avgHP[1]);
+         Debug.Log("ATK average de A: " + avgATK[0] + ", y de B: " + avgATK[1]);
+         Debug.Log("Melees de A: " + melee[0] + ", rangeds: " + ranged[0] + ", scouts: " + scouts[0] + ", y artilleria: " + artill[0]);
+         Debug.Log("Melees de B: " + melee[1] + ", rangeds: " + ranged[1] + ", scouts: " + scouts[1] + ", y artilleria: " + artill[1]);*/
 
         //Sacar una formula para calcular ese numero a devolver en base a los datos
+
+        Vector2 strength = new Vector2(0, 0);
+        strength[0] = totalHP[0] * dmpf[0]; // Considerar usar de algun modo el daño medio y el nº de unidades
+        strength[1] = totalHP[1] * dmpf[1];
+
+        if (fact == Faction.A)
+            return strength[0] - strength[1]; //Un numero positivo indica ventaja del bando A, un numero negativo indica ventaja del bando B
+        else
+            return strength[1] - strength[0]; //Como antes pero al reves
     }
 
     //Funciones que trabajan con influencia:
@@ -195,4 +255,5 @@ public class InfoManager : MonoBehaviour {
 
         return nodes;
     }
+
 }
--- Strategy/StrategyLayer.cs
+++ Strategy/StrategyLayer.cs
@@ -8,6 +8,8 @@ public enum Strategy {
 
 public class StrategyLayer : MonoBehaviour {
 
+    public LayerMask unitsMask;
+
     Dictionary<Strategy, float> weights = new Dictionary<Strategy, float>() { { Strategy.DEF_BASE, 0 },
                                                                               { Strategy.DEF_HALF, 0 },
                                                                               { Strategy.ATK_BASE, 0 },
@@ -18,10 +20,25 @@ public class StrategyLayer : MonoBehaviour {
     [SerializeField]
     Faction faction;
 
+    string mapSide;
+    Faction enemFac;
+
     // Use this for initialization
     void Start () {
         info = GetComponent<InfoManager>();
-	}
+
+        if (faction == Faction.A)
+        {
+            enemFac = Faction.B;
+            mapSide = "up";
+        }
+        else
+        {
+            enemFac = Faction.A;
+            mapSide = "down";
+        }
+            
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -42,12 +59,6 @@ public class StrategyLayer : MonoBehaviour {
         // Desde el centro de la base, un radio de 55 cubre todo el territorio
         // 40 sería un "casi llegando a la base"
         // 25 sería que están en la misma base
-        Faction enemFac;
-
-        if (faction == Faction.A)
-            enemFac = Faction.B;
-        else
-            enemFac = Faction.A;
 
         HashSet<AgentUnit> near = info.UnitsNearBase(faction, enemFac, 25);
         HashSet<AgentUnit> mid = info.UnitsNearBase(faction, enemFac, 40);
@@ -72,7 +83,91 @@ public class StrategyLayer : MonoBehaviour {
     }
 
     float WeightDefhalf() {
+
+        //TODO: Una a una, comprobar que funcionan las tres partes
+
+        float[] result = new float[3]; //0 = mid, 1 = top, 2 = bottom
+        // Separamos los tres waypoints para asegurarnos de tratar el caso de cada waypoint por separado y no mezclar resultados
+
+        // Comprobamos el numero de unidades enemigas cerca de cada waypoint. A más, más interesante será tomar esta estrategia
+        /*HashSet<AgentUnit> nearMid = info.GetUnitsFactionArea(info.waypointNode[info.waypoints["mid"]], 10, unitsMask, enemFac);
+        HashSet<AgentUnit> farMid = info.GetUnitsFactionArea(info.waypointNode[info.waypoints["mid"]], 25, unitsMask, enemFac);
+
+        farMid.ExceptWith(nearMid);
+
+        HashSet<AgentUnit> nearTop = info.GetUnitsFactionArea(info.waypointNode[info.waypoints["top"]], 10, unitsMask, enemFac);
+        HashSet<AgentUnit> farTop = info.GetUnitsFactionArea(info.waypointNode[info.waypoints["top"]], 25, unitsMask, enemFac);
+
+        farTop.ExceptWith(nearTop);
+
+        HashSet<AgentUnit> nearBottom = info.GetUnitsFactionArea(info.waypointNode[info.waypoints["bottom"]], 10, unitsMask, enemFac);
+        HashSet<AgentUnit> farBottom = info.GetUnitsFactionArea(info.waypointNode[info.waypoints["bottom"]], 25, unitsMask, enemFac);
+
+        farBottom.ExceptWith(nearBottom);*/
+
+        // El waypoint central cuenta más, ya que es el más importante
+        result[0] = WeightUnitsNearWaypoint("mid", 0.5f, 0.4f);
+        result[1] = WeightUnitsNearWaypoint("top", 0.4f, 0.3f);
+        result[2] = WeightUnitsNearWaypoint("bottom", 0.4f, 0.3f);
+        /* result[0] = (0.5f / 20) * nearMid.Count + (0.4f / 20) * farMid.Count;
+         result[1] = (0.4f / 20) * nearTop.Count + (0.3f / 20) * farTop.Count;
+         result[2] = (0.4f / 20) * nearBottom.Count + (0.3f / 20) * farBottom.Count;*/
+
+        // Ahora comprobamos la influencia aliada cerca del waypoint (pero no exactamente en el waypoint). Si el enemigo tiene mucha más influencia,
+        // habría quizá que reconsiderar que el camino es peligroso
+        /*float allyInfl = info.GetAreaInfluence(faction, info.waypointNode[info.waypoints[mapSide+"Mid"]]);
+        float enemyInfl = info.GetAreaInfluence(enemFac, info.waypointNode[info.waypoints[mapSide+"Mid"]]);*/
+
+  //      result[0] -= WeightAllyInfluenceWaypoint("Mid"); 
+  //      result[1] -= WeightAllyInfluenceWaypoint("Top");
+  //      result[2] -= WeightAllyInfluenceWaypoint("Bottom");
+
+        /*allyInfl = info.GetAreaInfluence(faction, info.waypointNode[info.waypoints[mapSide + "Top"]]);
+        enemyInfl = info.GetAreaInfluence(enemFac, info.waypointNode[info.waypoints[mapSide + "Top"]]);
+
+        result[1] -= Mathf.Min(0.2f, enemyInfl - allyInfl);
+
+        allyInfl = info.GetAreaInfluence(faction, info.waypointNode[info.waypoints[mapSide + "Bottom"]]);
+        enemyInfl = info.GetAreaInfluence(enemFac, info.waypointNode[info.waypoints[mapSide + "Bottom"]]);
+
+        result[2] -= Mathf.Min(0.2f, enemyInfl - allyInfl);*/
+
+  //      float area = 40;
+
+        // En el mejor caso para nosotros, tendremos un +0.5 en mid, que significa que al menos doblamos en fuerza al enemigo. En el peor, -0.35,
+        // ya que es probable que el enemigo se vea limitado por el terreno
+  //      float inflM = Mathf.Max(Mathf.Min(info.MilitaryAdvantage(info.waypointNode[info.waypoints["mid"]], area, unitsMask, faction), 0.5f),-0.35f);
+        // Para los otros dos waypoints, el resultado será menos relevante
+  //      float inflT = Mathf.Max(Mathf.Min(info.MilitaryAdvantage(info.waypointNode[info.waypoints["top"]], area, unitsMask, faction), 0.35f), -0.25f);
+  //      float inflB = Mathf.Max(Mathf.Min(info.MilitaryAdvantage(info.waypointNode[info.waypoints["bottom"]], area, unitsMask, faction), 0.35f), -0.25f);
+
+
+   //     result[0] += inflM;
+   //     result[1] += inflT;
+   //     result[2] += inflB;
+
         return 0;
+    }
+
+    float WeightUnitsNearWaypoint(string waypoint, float nearMult, float farMult)
+    {
+
+        HashSet<AgentUnit> near = info.GetUnitsFactionArea(info.waypointNode[info.waypoints[waypoint]], 10, unitsMask, enemFac);
+        HashSet<AgentUnit> far = info.GetUnitsFactionArea(info.waypointNode[info.waypoints[waypoint]], 25, unitsMask, enemFac);
+
+        far.ExceptWith(near);
+
+        float result = (nearMult / 20) * near.Count + (farMult / 20) * far.Count;
+
+        return result;
+    }
+
+    float WeightAllyInfluenceWaypoint(string waypoint) //TODO: Cambiar los nombres para que sean "Upmid", por ejemplo
+    {
+        float allyInfl = info.GetAreaInfluence(faction, info.waypointNode[info.waypoints[mapSide + waypoint]]);
+        float enemyInfl = info.GetAreaInfluence(enemFac, info.waypointNode[info.waypoints[mapSide + waypoint]]);
+
+        return Mathf.Min(0.2f, enemyInfl - allyInfl); // Preferimos que la influencia en nuestro lado sea nuestra
     }
 
     float WeightAtkhalf() {

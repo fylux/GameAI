89d12fe8de4ba4773214f442612a102bd5b75e84
Alfonso <alfonso.cama@hotmail.com>
2019-04-11 10:23:38 +0200

    Fixed error renaming StrategyT

--- Strategy/MilitaryResourcesAllocator.cs
+++ Strategy/MilitaryResourcesAllocator.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 
 public class MilitaryResourcesAllocator {
 
-    Dictionary<Strategy, float> weights; //let's better call it priority or interest, weight is another thing
-    Dictionary<Strategy, Color> strategyColor = new Dictionary<Strategy, Color>() {
-            { Strategy.ATK_BASE, Color.magenta},
-            { Strategy.ATK_HALF, Color.red},
-            { Strategy.DEF_BASE, Color.green},
-            { Strategy.DEF_HALF,Color.cyan}
+    Dictionary<StrategyT, float> weights; //let's better call it priority or interest, weight is another thing
+    Dictionary<StrategyT, Color> strategyColor = new Dictionary<StrategyT, Color>() {
+            { StrategyT.ATK_BASE, Color.magenta},
+            { StrategyT.ATK_HALF, Color.red},
+            { StrategyT.DEF_BASE, Color.green},
+            { StrategyT.DEF_HALF,Color.cyan}
         };
 
     public void AllocateResources() {
-        weights = new Dictionary<Strategy, float>() {
-            { Strategy.ATK_BASE, 0.5f},
-            { Strategy.ATK_HALF, 0.5f},
-            { Strategy.DEF_BASE, 0.8f},
-            { Strategy.DEF_HALF, 0.2f}
+        weights = new Dictionary<StrategyT, float>() {
+            { StrategyT.ATK_BASE, 0.5f},
+            { StrategyT.ATK_HALF, 0.5f},
+            { StrategyT.DEF_BASE, 0.8f},
+            { StrategyT.DEF_HALF, 0.2f}
         };
         int nTotalAvailableUnits = Map.unitList.Count;
 
-        foreach (Strategy strategy in weights.Keys.ToList()) {
+        foreach (StrategyT strategy in weights.Keys.ToList()) {
             if (weights[strategy] < 0.2) {
                 weights.Remove(strategy);
             }
@@ -31,24 +31,24 @@ public class MilitaryResourcesAllocator {
 
         //Normalize
         float sum = weights.Sum(w => w.Value);
-        foreach (Strategy strategy in weights.Keys.ToList()) {
+        foreach (StrategyT strategy in weights.Keys.ToList()) {
             weights[strategy] /= sum;
 
         }
 
         //Map to number of units
         //We ignore rounding of float, we just assign till there are available
-        Dictionary<Strategy, int> nUnitsAllocToStrategy = new Dictionary<Strategy, int>();
-        foreach (Strategy strategy in weights.Keys.ToList()) {
-            Debug.Log("Strategy " + strategy.ToString() + " units " + weights[strategy] * nTotalAvailableUnits);
-            nUnitsAllocToStrategy[strategy] = (int) (weights[strategy] * nTotalAvailableUnits);
+        Dictionary<StrategyT, int> nUnitsAllocToStrategy = new Dictionary<StrategyT, int>();
+        foreach (StrategyT strategy in weights.Keys.ToList()) {
+            Debug.Log("StrategyT " + strategy.ToString() + " units " + weights[strategy] * nTotalAvailableUnits);
+            nUnitsAllocToStrategy[strategy] = (int)(weights[strategy] * nTotalAvailableUnits);
         }
         Debug.Assert(nUnitsAllocToStrategy.Sum(w => w.Value) <= nTotalAvailableUnits);
 
-        Dictionary<Strategy, HashSet<AgentUnit>> unitsAssignedToStrategy = new Dictionary<Strategy, HashSet<AgentUnit>>();
+        Dictionary<StrategyT, HashSet<AgentUnit>> unitsAssignedToStrategy = new Dictionary<StrategyT, HashSet<AgentUnit>>();
         HashSet<AgentUnit> availableUnits = new HashSet<AgentUnit>(Map.unitList);
 
-        foreach (Strategy strategy in weights.Keys.ToList()) {
+        foreach (StrategyT strategy in weights.Keys.ToList()) {
             unitsAssignedToStrategy[strategy] = new HashSet<AgentUnit>();
             //Get list of units with "strategy assigned"
             List<AgentUnit> unitsWithStrategy = availableUnits.Where(unit => unit.strategy == strategy).ToList();
@@ -72,7 +72,7 @@ public class MilitaryResourcesAllocator {
         //Asign units randomly
         HashSet<AgentUnit> assignedUnits = new HashSet<AgentUnit>();
         foreach (AgentUnit unit in availableUnits) {
-            foreach (Strategy strategy in weights.Keys.ToList()) {
+            foreach (StrategyT strategy in weights.Keys.ToList()) {
                 if (nUnitsAllocToStrategy[strategy] > 0) {
                     unitsAssignedToStrategy[strategy].Add(unit);
                     assignedUnits.Add(unit);
@@ -84,14 +84,14 @@ public class MilitaryResourcesAllocator {
         availableUnits.ExceptWith(assignedUnits);
 
         //There might be still available units due to rounding errors, they will be assigned to the most weighted strategy
-        Strategy mostImportantStrategy = weights.FirstOrDefault(strategy => strategy.Value == weights.Values.Max()).Key;
+        StrategyT mostImportantStrategy = weights.FirstOrDefault(strategy => strategy.Value == weights.Values.Max()).Key;
         unitsAssignedToStrategy[mostImportantStrategy].UnionWith(availableUnits);
         availableUnits.Clear();
         Console.Log("Most important Strategy: " + mostImportantStrategy.ToString());
 
 
-        foreach (Strategy strategy in weights.Keys.ToList()) {
-            Console.Log("Strategy: "+strategy.ToString()+" = "+ unitsAssignedToStrategy[strategy].Count + " units");
+        foreach (StrategyT strategy in weights.Keys.ToList()) {
+            Console.Log("Strategy: " + strategy.ToString() + " = " + unitsAssignedToStrategy[strategy].Count + " units");
 
             foreach (AgentUnit unit in unitsAssignedToStrategy[strategy]) {
                 unit.GetComponent<Renderer>().material.color = strategyColor[strategy];
@@ -100,7 +100,7 @@ public class MilitaryResourcesAllocator {
     }
 
 
-    public void SetWeights(Dictionary<Strategy, float> weights) {
+    public void SetWeights(Dictionary<StrategyT, float> weights) {
         this.weights = weights;
     }
 }

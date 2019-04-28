4be4ff404d5ba7285b7ad7182b6a3378b1bfdf46
Alfonso <alfonso.cama@hotmail.com>
2019-04-28 18:04:28 +0200

    Abstraction HostileTask

---
+++ Actions/HostileTask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class HostileTask : Task {
+
+    public HostileTask(AgentUnit agent, Action<bool> callback) : base(agent, callback) {}
+
+    public abstract void ReceiveAttack(AgentUnit enemy);
+}

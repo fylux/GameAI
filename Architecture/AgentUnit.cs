97cc7f4bf1b3af88b60a6fdb9d3864dd2bf8c611
Jaime <jaime.m.legaz.306@gmail.com>
2019-05-03 22:56:19 +0200

    Atkhalf doesnt give Null Exception anymore

--- Architecture/AgentUnit.cs
+++ Architecture/AgentUnit.cs
@@ -125,6 +125,6 @@ public abstract class AgentUnit : AgentNPC {
 
     public float GetDropOff(float locationDistance) {
         //100f is 100% influence
-        return 1000f / (locationDistance*locationDistance);
+        return 775f / (locationDistance*locationDistance);
     }
 }

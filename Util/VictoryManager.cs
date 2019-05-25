7e0f8b679efac5f86592ef4c7a6b0a027917db79
Jaime <jaime.m.legaz.306@gmail.com>
2019-05-25 19:12:30 +0200

    Victory condition added

---
+++ Util/VictoryManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryManager : MonoBehaviour {
+
+	[SerializeField]
+	MainBase downBase, upBase;
+
+	[SerializeField]
+	GameObject blue, red;
+
+	bool victory = false;
+
+	void Update () {
+		if (downBase.health <= 0 && !victory) {
+			red.SetActive (true);
+			victory = true;
+		}
+		else if (upBase.health <= 0 && !victory) {
+			blue.SetActive (true);
+			victory = true;
+		}		
+	}
+}
